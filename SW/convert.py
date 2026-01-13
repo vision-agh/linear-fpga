c98590c1bab4af6a46cb123998ec8aba4bd103f6
Dawid Podsiadlo <dpodsiadlo06@gmail.com>
2026-01-13 20:57:52 +0100

    nowy_save_weights

--- SW/convert.py
+++ SW/convert.py
@@ -49,15 +49,29 @@ class ToMem:
                 for j in i:
                     f.write(toBin(j, self.feature_prec)+"\n")
 
-    def saveWeights(self):
+    def saveWeights(self, split=1):
         assert len(self.weights) == len(self.superbias)
         with open("../HW/weights.mem", "w") as f:
-            for i in range(len(self.superbias)):
-                f.write(toHex(self.superbias[i], self.bias_prec)+"".join([toHex(j, self.feature_prec) for j in self.weights[i]])+"\n")
+            # for i in range(len(self.superbias)*split):
+            # f.write(toHex(self.superbias[i], self.bias_prec)+"".join([toHex(j, self.feature_prec) for j in self.weights[i]])+"\n")
+            weights_split = [np.array_split(i, split) for i in self.weights]
+            weights_split_flat = [x for xs in weights_split for x in xs]
+
+            tmp = ""
+            for i in range(len(weights_split_flat)):
+                if (i < len(self.superbias)):
+                    tmp += toHex(self.superbias[i], self.bias_prec)
+                else:
+                    tmp += toHex(np.int32(0), self.bias_prec)
+                tmp += "".join([toHex(j, self.feature_prec) for j in weights_split_flat[i]])
+                tmp += "\n"
+
+                f.write(tmp)
+                tmp = ""
 
 
 saver = ToMem(8, 32)
 saver.setSuperbias(np.abs(np.random.randint(low=0, high=2**31-1, size=8, dtype=np.int32)))
 saver.setWeights(np.abs(np.random.randint(low=0, high=2**7-1, size=(8, 8), dtype=np.int8)))
 
-saver.saveWeights()
+saver.saveWeights(4)
